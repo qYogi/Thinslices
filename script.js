abd1ea6324112917b02698331f9f89ef90433549
qYogi <bogdanviziru18@gmail.com>
2024-07-11 14:13:27 +0300

    added

---
+++ script.js
@@ -0,0 +1,28 @@
+const xButton = document.getElementById('X');
+const oButton = document.getElementById('O');
+
+const menu = document.querySelector('.menu')
+const newGame = document.getElementById('CPU');
+const newGame2 = document.getElementById('Player');
+
+let currentPlayer = null;
+
+xButton.addEventListener('click', () => {
+    currentPlayer = 'X';
+    xButton.querySelector('svg path').setAttribute('fill', '#F2B137');
+    oButton.querySelector('svg path').setAttribute('fill', '#1A2A33');
+
+});
+
+oButton.addEventListener('click', () => {
+    currentPlayer = 'O';
+    oButton.querySelector('svg path').setAttribute('fill', '#F2B137');
+    xButton.querySelector('svg path').setAttribute('fill', '#A8BFC9');
+});
+
+newGame.addEventListener('click', () => {
+    menu.style.display = 'none';
+})
+newGame2.addEventListener('click', () => {
+    menu.style.display = 'none';
+})
